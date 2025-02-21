934bd134e2ae214a967ca79b87b558e3912368ab
askusWT <63749667+askusWT@users.noreply.github.com>
2025-02-21 03:34:42 +0000

    Update base16.nix (#3)

--- config/colorschemes/base16.nix
+++ config/colorschemes/base16.nix
@@ -3,15 +3,11 @@
   options = {
     base16.enable = lib.mkEnableOption "Enable base16 module";
   };
-# Always define colorschemes.base16
+
   config = {
     colorschemes.base16 = {
-      enable = false;
+      enable = true;
       colorscheme = "mountain";
     };
-  }
-  // lib.mkIf config.base16.enable {
-    # If user sets base16.enable = true, we override "enable = false" to true
-    colorschemes.base16.enable = true;
   };
 }
