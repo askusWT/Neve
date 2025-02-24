0ef108301c70af3f3849d250d917134180b941a1
askusWT <63749667+askusWT@users.noreply.github.com>
2025-02-24 22:19:26 +0000

    home-manager patch

--- flake.nix
+++ flake.nix
@@ -8,37 +8,34 @@
 
   outputs = { self, nixpkgs, nixvim, flake-utils, ... }@inputs:
     let
-      config = import ./config;  # Neovim module configuration
+      config = import ./config;
       nixpkgsConfig = { allowUnfree = true; };
     in
-    {
-      # ✅ Home Manager module export (fixes previous issue)
-      homeManagerModules.default = config;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        nixvimLib = nixvim.lib.${system};
+        pkgs = import nixpkgs { inherit system; config = nixpkgsConfig; };
+        nixvim' = nixvim.legacyPackages.${system};
+        nvim = nixvim'.makeNixvimWithModule {
+          inherit pkgs;
+          module = config;
+        };
+      in
+      {
+        checks.default = nixvimLib.check.mkTestDerivationFromNvim {
+          inherit nvim;
+          name = "Neve";
+        };
 
-      # ✅ Nixvim module export (for NixOS & flake builds)
-      nixvimModule = config;
+        packages.default = nvim;
 
-      # ✅ System-wide outputs for `nix build` and `nix run`
-      packages = flake-utils.lib.eachDefaultSystem (system:
-        let
-          pkgs = import nixpkgs { inherit system; config = nixpkgsConfig; };
-          nixvim' = nixvim.legacyPackages.${system};
-          nvim = nixvim'.makeNixvimWithModule {
-            inherit pkgs;
-            module = config;
-            extraSpecialArgs = { inherit self; };
-          };
-        in
-        {
-          checks.default = nixvim.lib.${system}.check.mkTestDerivationFromNvim {
-            inherit nvim;
-            name = "Neve";
-          };
+        formatter = pkgs.nixfmt-rfc-style;
 
-          packages.default = nvim;
+        #  New: Home Manager Module (Only Addition)
+        homeManagerModules.default = config;
 
-          formatter = pkgs.nixfmt-rfc-style;
-        }
-      );
-    };
+        nixvimModule = config;
+      }
+    );
 }
+
